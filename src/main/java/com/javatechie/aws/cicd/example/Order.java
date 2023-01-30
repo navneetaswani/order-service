f6869c200d03797b4157af9201005fc79cc5d01a
navneetgmail <navneetaswani@gmail.com>
2023-01-31 01:34:29 +0530

    Buildspec

--- src/main/java/com/javatechie/aws/cicd/example/Order.java
+++ src/main/java/com/javatechie/aws/cicd/example/Order.java
@@ -1,17 +1,46 @@
 package com.javatechie.aws.cicd.example;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Data
+/*@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@ToString*/
 public class Order {
 
-    private int id;
+    public int getId() {
+		return id;
+	}
+	public Order() {
+		super();
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}
+	public Order(int id, String name, int quantity, long price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	private int id;
     private String name;
     private int quantity;
     private long price;
